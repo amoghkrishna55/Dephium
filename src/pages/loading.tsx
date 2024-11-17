59be2299881562c945b3ef3460097e9ec9799cb4
Amogh Krishna <amoghkrishna55@gmail.com>
2024-11-17 13:22:04 +0530

    Refactor loading component to simplify loading spinner logic

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -10,9 +10,7 @@ const Loading = ({ onVideoLoad }: LoadingProps) => {
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      {!localLoaded && (
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary" />
-      )}
+      {!localLoaded && <div />}
       <video
         autoPlay
         loop
