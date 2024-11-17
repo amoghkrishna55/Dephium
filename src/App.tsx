2c6bcec78a9093ee318c7391938dae0c9afe35ba
Amogh Krishna <amoghkrishna55@gmail.com>
2024-11-17 15:27:01 +0530

    Implement routing in App component for main, select, and meme review pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/main";
+import Select from "./pages/select";
+import MemeReview from "./games/memeReview";
 
 const App = () => {
-  return <Main />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/select" element={<Select />} />
+        <Route path="/game/meme-review" element={<MemeReview />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Select from "./pages/select.tsx";
-import MemeReview from "./games/memeReview.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/select",
-    element: <Select />,
-  },
-  {
-    path: "/game/meme-review",
-    element: <MemeReview />,
-  },
-]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
